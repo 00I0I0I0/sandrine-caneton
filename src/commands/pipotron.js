b9fae10001d2736a468955446b93f3f5f437e7c8
00101010 <00101010@00101010.fr>
2020-12-24 02:14:00 +0100

    ajout du pipotron (c'est la faute a greg)

--- src/commands/pipotron.js
+++ src/commands/pipotron.js
@@ -1,19 +1,41 @@
 const { Logger } = require('logger');
 const { splitToPlayable } = require('../common/utils');
 const allowOver200 = process.env.ALLOW_OVER_200 || require('../../config/settings.json').allow_more_than_200_chars;
-
+const pipotron_list = require('../../data/pipotron.json')
 const logger = new Logger();
 
 module.exports = {
-  name: 'say',
+  name: 'pipotron',
   group: "user",
-  description: `Send a TTS message in your voice channel${allowOver200 ? '.' : ' (Up to 200 characters).'}`,
-  emoji: ':speaking_head:',
+  description: `pipotron`,
+  emoji: ':juggling:',
   execute(message, options) {
     const { channel } = message.member.voice;
     const { ttsPlayer, name: guildName, voice } = message.guild;
     const connection = voice ? voice.connection : null;
+	  options.args = "";
+	  var d = false
+	  for (let i = 0; i < pipotron_list.length; i++)
+	  {
+		  tmp = pipotron_list[i][Math.floor(Math.random() * pipotron_list[i].length)]
+		  if (d == true)
+		  {
+			if ("aeiouéè".indexOf(tmp[0]) > -1)
+				tmp = "d'" + tmp
+			else
+				tmp = "de " + tmp
+ 			d = false
+		  }
+		  if (tmp[tmp.length - 1] == '#')
+		  {
+			d = true
+		  	tmp = tmp.substring(0,tmp.length - 3)
+		  }
+		  options.args += tmp + " "
+	  }
+	  options.args = options.args.split(" ")
     const [atLeastOneWord] = options.args;
+
     if (!channel) {
       message.reply('you need to be in a voice channel first.');
       return;
@@ -28,7 +50,6 @@ module.exports = {
       message.reply('you need to specify a message.');
       return;
     }
-
     if (connection) {
       splitToPlayable(options.args)
         .then((phrases) => {
